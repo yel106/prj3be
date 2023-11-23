075219c79f1e0a55e6d32bd3911dd6335df7d29c
yel106 <yel_@hanmail.net>
2023-11-23 14:46:15 +0900

    initial commit

---
+++ src/main/java/com/example/prj3be/Prj3beApplication.java
@@ -0,0 +1,13 @@
+package com.example.prj3be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Prj3beApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Prj3beApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/prj3be/Prj3beApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.prj3be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Prj3beApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
