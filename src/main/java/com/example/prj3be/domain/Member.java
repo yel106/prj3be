c49672aa816e841f780280a6b682bf2111e03af9
kkssii0210 <kkssii0210@naver.com>
2023-11-24 11:54:27 +0900

    Member에 관한 CRUD (D는 아직안함) 멤버 단건 조회와 전체 조회도 만듬

--- src/main/java/com/example/prj3be/domain/Member.java
+++ src/main/java/com/example/prj3be/domain/Member.java
@@ -1,4 +1,35 @@
 package com.example.prj3be.domain;
 
-public class Member {
+import com.example.prj3be.constant.Role;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name="member")
+@Getter
+@Setter
+public class Member extends BaseTimeEntity{
+    @Id
+    @Column(name="member_id")
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String logId; //로그인용 아이디
+    private String password;
+    private String name;
+    private String address;
+    private String email;
+
+    private Integer age;
+
+    private String gender; // 주민등록번호 뒤 첫번째 숫자.
+
+    @Enumerated(EnumType.STRING)
+    private Role role; //ADMIN,USER
+
+
+    public Member() {
+    }
+
 }
