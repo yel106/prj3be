673cf7588570ce163ee8da738621f363b074c3f8
Won A Young <zeronediary@naver.com>
2023-11-24 15:35:21 +0900

    member에서 order와 일대다 연관관계 매핑

--- src/main/java/com/example/prj3be/domain/Order.java
+++ src/main/java/com/example/prj3be/domain/Order.java
@@ -14,9 +14,9 @@ public class Order {
     @Column(name="order_id")
     private Long id;
 
-//    @ManyToOne
-//    @JoinColumn(name="member_id")
-//    private Member member;
+    @ManyToOne
+    @JoinColumn(name="member_id")
+    private Member member;
 
     @OneToMany(mappedBy = "order")
     private List<OrderItem> orderItems = new ArrayList<OrderItem>();
