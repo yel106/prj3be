469d1ee8f50fbcd57a26a7f1e19ef004dc9b20fe
kkssii0210 <kkssii0210@naver.com>
2023-11-24 11:54:27 +0900

    Member에 관한 CRUD (D는 아직안함) 멤버 단건 조회와 전체 조회도 만듬

--- src/main/java/com/example/prj3be/service/MemberService.java
+++ src/main/java/com/example/prj3be/service/MemberService.java
@@ -1,4 +1,46 @@
 package com.example.prj3be.service;
 
+import com.example.prj3be.domain.Member;
+import com.example.prj3be.dto.MemberEditFormDto;
+import com.example.prj3be.repository.MemberRepository;
+import jakarta.persistence.EntityNotFoundException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+@Transactional
 public class MemberService {
+    private final MemberRepository memberRepository;
+    public void signup(Member member) {
+        memberRepository.save(member);
+    }
+
+    public Member findMemberById(Long id) {
+        Optional<Member> findMember1 = memberRepository.findById(id);
+        Member member = findMember1.get();
+        return member;
+    }
+
+    public void update(Long id, MemberEditFormDto dto) {
+        Member member = memberRepository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException("Member not found with id: " + id));
+        member.setPassword(dto.getPassword());
+        member.setEmail(dto.getEmail());
+        member.setAddress(dto.getAddress());
+    }
+
+    public String getEmail(String email) {
+        return memberRepository.findEmailByEmail(email)
+                .orElse(null);
+    }
+
+    public List<Member> findMemberList() {
+        List<Member> all = memberRepository.findAll();
+        return all;
+    }
 }
