81ac8327ba1ba026575fc54a3f454965d93fd0a1
jeon602 <731aa@naver.com>
2023-11-27 11:51:22 +0900

    item domain error

--- src/main/java/com/example/prj3be/domain/Board.java
+++ src/main/java/com/example/prj3be/domain/Board.java
@@ -22,14 +22,14 @@ public class Board {
     @OneToMany(mappedBy = "board")
     private List<Item> items = new ArrayList<>();
 
-    @OneToMany(mappedBy = "comment")
+
+    @OneToMany(mappedBy = "board")
     private List<Comment> comments = new ArrayList<>();
 
-    public Board(Long id, List<Item> items) {
+    public Board(Long id, String content, List<Item> items, List<Comment> comments) {
         this.id = id;
+        this.content = content;
         this.items = items;
+        this.comments = comments;
     }
-
-
-
 }
