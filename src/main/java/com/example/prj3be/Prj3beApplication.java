69d760d4cefe532d016ef4400e368b7992338481
yel106 <yel_@hanmail.net>
2023-11-27 15:49:36 +0900

    커밋

--- src/main/java/com/example/prj3be/Prj3beApplication.java
+++ src/main/java/com/example/prj3be/Prj3beApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@EnableJpaAuditing
+//@EnableJpaAuditing -> 중복으로 쓰면 안 됨
 @SpringBootApplication
 public class Prj3beApplication {
 
