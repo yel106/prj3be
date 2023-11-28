89f0361a1a5f62523bfdacc1e2c67078ef9ef36e
Won A Young <zeronediary@naver.com>
2023-11-28 15:38:01 +0900

    jwt 커스텀 필터인 JwtFilter 생성

---
+++ src/main/generated/com/example/prj3be/domain/QMember.java
@@ -0,0 +1,64 @@
+package com.example.prj3be.domain;
+
+import static com.querydsl.core.types.PathMetadataFactory.*;
+
+import com.querydsl.core.types.dsl.*;
+
+import com.querydsl.core.types.PathMetadata;
+import javax.annotation.processing.Generated;
+import com.querydsl.core.types.Path;
+import com.querydsl.core.types.dsl.PathInits;
+
+
+/**
+ * QMember is a Querydsl query type for Member
+ */
+@Generated("com.querydsl.codegen.DefaultEntitySerializer")
+public class QMember extends EntityPathBase<Member> {
+
+    private static final long serialVersionUID = -341639019L;
+
+    public static final QMember member = new QMember("member1");
+
+    public final QBaseTimeEntity _super = new QBaseTimeEntity(this);
+
+    public final StringPath address = createString("address");
+
+    public final NumberPath<Integer> age = createNumber("age", Integer.class);
+
+    public final StringPath email = createString("email");
+
+    public final StringPath gender = createString("gender");
+
+    public final NumberPath<Long> id = createNumber("id", Long.class);
+
+    public final StringPath logId = createString("logId");
+
+    public final StringPath name = createString("name");
+
+    public final ListPath<Order, QOrder> orders = this.<Order, QOrder>createList("orders", Order.class, QOrder.class, PathInits.DIRECT2);
+
+    public final StringPath password = createString("password");
+
+    //inherited
+    public final DateTimePath<java.time.LocalDateTime> regTime = _super.regTime;
+
+    public final EnumPath<com.example.prj3be.constant.Role> role = createEnum("role", com.example.prj3be.constant.Role.class);
+
+    //inherited
+    public final DateTimePath<java.time.LocalDateTime> updateTime = _super.updateTime;
+
+    public QMember(String variable) {
+        super(Member.class, forVariable(variable));
+    }
+
+    public QMember(Path<? extends Member> path) {
+        super(path.getType(), path.getMetadata());
+    }
+
+    public QMember(PathMetadata metadata) {
+        super(Member.class, metadata);
+    }
+
+}
+
