ee7bb6a6857f38e8df4842e567fc737828e69855
Won A Young <zeronediary@naver.com>
2023-11-28 16:27:30 +0900

    401 에러를 리턴하는 JwtAuthenticationEntryPoint 생성

---
+++ src/main/java/com/example/prj3be/jwt/JwtAuthenticationEntryPoint.java
@@ -0,0 +1,22 @@
+package com.example.prj3be.jwt;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+// 유효한 자격 증명을 제공하지 않고 접근하려 할 때 401 Unauthorized 에러 리턴
+@Component
+public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
+    @Override
+    public void commence(HttpServletRequest request,
+                         HttpServletResponse response,
+                         AuthenticationException authException) throws IOException {
+        // 유효한 자격 증명을 제공하지 않고 접근하려 할 때 401
+        response.sendError(HttpServletResponse.SC_UNAUTHORIZED);
+    }
+}
