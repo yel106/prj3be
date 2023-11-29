9b0d470e174024928850b07922c780424500189d
smil98 <seungmilee98@gmail.com>
2023-11-29 17:08:32 +0900

    카카오 로그인 연동을 위한 컨트롤러 작성

--- src/main/java/com/example/prj3be/controller/LoginController.java
+++ src/main/java/com/example/prj3be/controller/LoginController.java
@@ -14,10 +14,9 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
 
 @RestController
 public class LoginController {
@@ -67,5 +66,35 @@ public class LoginController {
     public ResponseEntity<String> socialButtonImage() {
         return ResponseEntity.ok(socialButtonImagePrefix);
     }
+
+    @ResponseBody
+    @GetMapping("/api/login/kakao")
+    public BaseResponse<PostLoginRes> kakaoLogin(@RequestParam(required = false) String code) {
+        try{
+            //URL에 포함된 code를 이용하여 액세스 토큰 발급
+            String accessToken = loginService.getKakaoAccessToken(code);
+            System.out.println("accessToken = " + accessToken);
+
+            //액세스 토큰을 이용하여 카카오 서버에서 유저 정보(닉네임, 이메일) 받아오기
+            HashMap<String, Object> userInfo = loginService.getUserInfo(accessToken);
+            System.out.println("userInfo = " + userInfo);
+
+            PostLoginRes postLoginRes = null;
+
+            //만약 DB에 해당 이메일을 가진 유저가 없다면 회원가입 시키고, 유저 식별자와 JWT 반환
+            //전화번호, 성별, 및 기타 개인 정보는 사업자 번호가 없기 때문에 받아올 권한이 없어 테스트 불가능
+            if(loginProvider.checkEmail(String.valueOf(userInfo.get("email"))) == 0) {
+                return new BaseResponse<>(postLoginRes);
+            } else {
+                //해당 이메일을 가진 유저가 있다면 기존 유저의 로그인으로 판단하고 유저 식별자와 JWT 반환
+                postLoginRes = loginProvider.getUserInfo(String.valueOf(userInfo.get("email")));
+                return new BaseResponse<>(postLoginRes);
+            }
+        } catch (BaseException exception) {
+            return new BaseResponse<>((exception.getStatus()));
+        }
+    }
+
+
 
 }
