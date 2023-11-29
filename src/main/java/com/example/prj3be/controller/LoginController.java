2357d8b669c1ab6e7c5c830bef8a7caa5ef8113a
smil98 <seungmilee98@gmail.com>
2023-11-29 15:40:42 +0900

    AuthController -> LoginController로 변경

--- src/main/java/com/example/prj3be/controller/LoginController.java
+++ src/main/java/com/example/prj3be/controller/LoginController.java
@@ -1,16 +1,67 @@
 package com.example.prj3be.controller;
 
+import com.example.prj3be.dto.LoginDto;
+import com.example.prj3be.dto.TokenDto;
+import com.example.prj3be.jwt.JwtFilter;
+import com.example.prj3be.jwt.TokenProvider;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class LoginController {
-
+    private final TokenProvider tokenProvider;
+    private final AuthenticationManagerBuilder authenticationManagerBuilder;
     @Value("${button.image.url}")
     private String socialButtonImagePrefix;
+
+    public LoginController(TokenProvider tokenProvider, AuthenticationManagerBuilder authenticationManagerBuilder){
+        this.tokenProvider = tokenProvider;
+        this.authenticationManagerBuilder = authenticationManagerBuilder;
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<TokenDto> authorize(@Valid @RequestBody LoginDto loginDto){
+        System.out.println("loginDto.getLogId() = " + loginDto.getLogId());
+        System.out.println("loginDto.getPassword() = " + loginDto.getPassword());
+
+        UsernamePasswordAuthenticationToken authenticationToken =
+                new UsernamePasswordAuthenticationToken(loginDto.getLogId(), loginDto.getPassword());
+
+        System.out.println("authenticationToken = " + authenticationToken);
+
+        try {
+            Authentication authentication = authenticationManagerBuilder.getObject().authenticate(authenticationToken);
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+
+            System.out.println("authentication = " + authentication);
+
+            String jwt = tokenProvider.createToken(authentication);
+
+            System.out.println("jwt = " + jwt);
+
+            HttpHeaders httpHeaders = new HttpHeaders();
+            httpHeaders.add(JwtFilter.AUTHORIZATION_HEADER, "Bearer " + jwt);
+
+            System.out.println("httpHeaders = " + httpHeaders);
+
+            return new ResponseEntity<>(new TokenDto(jwt), httpHeaders, HttpStatus.OK);
+        } catch (AuthenticationException e){
+            System.out.println("인증 실패 :"+e.getMessage());
+            return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
+        }
+    }
 
     @GetMapping("/api/login/image")
     public ResponseEntity<String> socialButtonImage() {
