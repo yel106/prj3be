1cbc3ca691959cb7a7c818452572d582fde8d2a3
Won A Young <zeronediary@naver.com>
2023-12-08 10:41:48 +0900

    merge 중 ...

--- src/main/generated/com/example/prj3be/domain/QMember.java
+++ src/main/generated/com/example/prj3be/domain/QMember.java
@@ -7,7 +7,6 @@ import com.querydsl.core.types.dsl.*;
 import com.querydsl.core.types.PathMetadata;
 import javax.annotation.processing.Generated;
 import com.querydsl.core.types.Path;
-import com.querydsl.core.types.dsl.PathInits;
 
 
 /**
@@ -37,8 +36,6 @@ public class QMember extends EntityPathBase<Member> {
     public final StringPath logId = createString("logId");
 
     public final StringPath name = createString("name");
-
-    public final ListPath<Order, QOrder> orders = this.<Order, QOrder>createList("orders", Order.class, QOrder.class, PathInits.DIRECT2);
 
     public final StringPath password = createString("password");
 
