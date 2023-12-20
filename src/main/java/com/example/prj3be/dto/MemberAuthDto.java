cea8ce8fba2c706a15122d6105e73adc294a91b7
yel106 <yel_@hanmail.net>
2023-12-20 18:22:05 +0900

    직렬화 해결중

--- src/main/java/com/example/prj3be/dto/MemberAuthDto.java
+++ src/main/java/com/example/prj3be/dto/MemberAuthDto.java
@@ -1,12 +1,20 @@
 package com.example.prj3be.dto;
 
-import lombok.Data;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.io.Serializable;
 
-@Data
+@Getter
+@Setter
 public class MemberAuthDto implements Serializable {
     private static final long serialVersionUID = 1L; // serialVersionUID 추가
     private String logId;
     private String role;
+    @JsonCreator
+    public MemberAuthDto() {
+        this.logId = logId;
+        this.role = role;
+    }
 }
