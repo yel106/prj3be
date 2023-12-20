7597f779f60ed057f7798501fc1469673bb4dd19
kkssii0210 <kkssii0210@naver.com>
2023-12-20 18:01:31 +0900

    직렬화 문제 해결중

--- src/main/java/com/example/prj3be/dto/MemberAuthDto.java
+++ src/main/java/com/example/prj3be/dto/MemberAuthDto.java
@@ -2,8 +2,11 @@ package com.example.prj3be.dto;
 
 import lombok.Data;
 
+import java.io.Serializable;
+
 @Data
-public class MemberAuthDto {
+public class MemberAuthDto implements Serializable {
+    private static final long serialVersionUID = 1L; // serialVersionUID 추가
     private String logId;
     private String role;
 }
