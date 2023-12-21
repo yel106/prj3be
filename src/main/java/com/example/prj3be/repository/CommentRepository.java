a46e1ed5fc0e1753ea61f13de8407b1dc1cf7349
yel106 <yel_@hanmail.net>
2023-12-21 19:31:03 +0900

    멤버 삭제시 코멘트 삭제2

--- src/main/java/com/example/prj3be/repository/CommentRepository.java
+++ src/main/java/com/example/prj3be/repository/CommentRepository.java
@@ -2,10 +2,14 @@ package com.example.prj3be.repository;
 
 import com.example.prj3be.domain.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 
 public interface CommentRepository extends JpaRepository<Comment, Long>, QuerydslPredicateExecutor<Comment> {
-    Long deleteCommentByMemberId(Long id);
+    @Modifying
+    @Query("DELETE FROM Comment c WHERE c.member.id = :memberId")
+    Long deleteCommentByMemberId(Long memberId);
 
     Long deleteCommentByBoardId(Long id);
 }
