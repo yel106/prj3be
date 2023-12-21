55672bbf7dd4f9e57cd12ce47f49c2437139cd3f
kkssii0210 <kkssii0210@naver.com>
2023-12-21 15:09:27 +0900

    토큰 갱신 해결중3

--- src/main/java/com/example/prj3be/config/CorsConfig.java
+++ src/main/java/com/example/prj3be/config/CorsConfig.java
@@ -10,20 +10,38 @@ import org.springframework.web.filter.CorsFilter;
 //CorsFilter 빈을 직접 정의
 @Configuration
 public class CorsConfig {
+//    @Bean
+//    public CorsFilter corsFilter(){
+//        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+//        CorsConfiguration config = new CorsConfiguration();
+//
+//        config.setAllowCredentials(true);
+//        // 출처
+//        config.addAllowedOriginPattern("*");
+//        config.addAllowedHeader("*");
+//        config.addAllowedMethod("*");
+//
+//        // 경로
+//        source.registerCorsConfiguration("/**", config);
+//        // => 모든 경로에 대해 모든 출처에서의 접근ㅎ 허용
+//        return new CorsFilter(source);
+//    }
+
     @Bean
-    public CorsFilter corsFilter(){
+    public CorsFilter corsFilter() {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         CorsConfiguration config = new CorsConfiguration();
 
-        config.setAllowCredentials(true);
-        // 출처
-        config.addAllowedOriginPattern("*");
-        config.addAllowedHeader("*");
-        config.addAllowedMethod("*");
+        config.setAllowCredentials(true); // 쿠키 및 인증 정보 허용
+        // 구체적인 출처 지정
+        config.addAllowedOrigin("https://muemalaf.click"); // 허용할 출처 지정
+        config.addAllowedHeader("*"); // 모든 헤더 허용
+        config.addAllowedMethod("*"); // 모든 HTTP 메소드 허용
 
-        // 경로
+        // 모든 경로에 대해 위에서 정의한 CORS 설정 적용
         source.registerCorsConfiguration("/**", config);
-        // => 모든 경로에 대해 모든 출처에서의 접근ㅎ 허용
+
         return new CorsFilter(source);
     }
+
 }
